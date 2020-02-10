d4b0e8172db0d3c8ba952af6f7732146e69243a2
Nate_River <crimson747@Nate-Riverui-MacBookPro.local>
2020-02-11 00:25:17 +0900

    tutorial_third_final

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,24 +5,22 @@ from .models import Question
 
 
 def index(request):
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("polls/index.html")
-    # context = {
-    #     "latest_question_list": latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404("Question does not exist")
-    # return render(request, "polls/detail.html", {"question": question})
-    question = get_object_or_404(Question, pk=question_id)
+
     return render(request, "polls/detail.html", {"question": question})
 
 
